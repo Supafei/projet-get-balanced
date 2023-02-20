2e2422fc49ed5f0b38c2fbb2f04cb834c8c8b230
BarbaraOuisse <barbaraouisse@hotmail.com>
2023-02-20 10:19:09 +0100

    create tables

---
+++ migration/1.create_tables.sql
@@ -0,0 +1,63 @@
+BEGIN;
+
+DROP TABLE
+    IF EXISTS user,
+    planner,
+    category,
+    task,
+    user_has_planner;
+
+CREATE DOMAIN email_address as text CHECK(VALUE ~ "^[a-z0-9][a-z_-!?.+0-9]*[a-z0-9]@[a-z]{1,63}\.[a-z]{2,63}$")
+
+CREATE TABLE
+    user (
+        id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        firstname text NOT NULL,
+        lastname text NOT NULL,
+        email text UNIQUE NOT NULL,
+        birthdate TIMESTAMPTZ,
+        avatar text,
+        password text NOT NULL,
+        color text NOT NULL DEFAULT "#b0b",
+        created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMPTZ
+    );
+
+CREATE TABLE
+    planner (
+        id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        name text NOT NULL,
+        description TEXT,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMPTZ
+    )
+CREATE TABLE
+    category (
+        id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        label text NOT NULL,
+        color text NOT NULL,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMPTZ
+    )
+CREATE TABLE
+    task (
+        id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        name text NOT NULL,
+        description text,
+        date TIMESTAMPTZ NOT NULL,
+        done BOOLEAN DEFAULT FALSE,
+        planner_id int REFERENCES planner(id) ON DELETE CASCADE,
+        category_id int REFERENCES category(id) ON DELETE CASCADE,
+        user_id int REFERENCES user(id) ON DELETE CASCADE,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMPTZ
+    )
+CREATE TABLE
+    user_has_planner (
+        id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        user_id int REFERENCES user(id) ON DELETE CASCADE,
+        planner_id int REFERENCES planner(id) ON DELETE CASCADE,
+        created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMPTZ
+    )
+COMMIT;
