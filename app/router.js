6d6fcc61fa4669e5dc073e79d49a4af60551a0dc
Fetra RABEHASY <94452120+Supafei@users.noreply.github.com>
2023-02-21 11:07:27 +0100

    création du dossier controller et des fichiers

--- app/router.js
+++ app/router.js
@@ -1,35 +1,39 @@
 const express = require("express");
+const { categoryController} = require ("../app/controller/categoryController");
+const { plannerController } = require ("../app/controller/plannerController");
+const { taskController } = require("../app/controller/taskController");
+const { userController } = require ("../app/controller/userController");
+
 
 const router = express.Router();
 
 
 /** ROUTES USER  */
-router.get("/",  ) // récupère le profil d'un user
-router.post("/user/login", ) // router pour logguer un utilisateur
-router.post("/",) // Ajoute un utilisateur en bdd
-router.put("/:id",) // modifier un utilisateur en bdd
-router.delete("/:id",  ) //supprime un utilisateur
+router.get("/", userController ) // récupère le profil d'un user
+router.post("/user/login", userController ) // router pour logguer un utilisateur
+router.post("/", userController) // Ajoute un utilisateur en bdd
+router.put("/:id", userController) // modifier un utilisateur en bdd
+router.delete("/:id", userController ) //supprime un utilisateur
 
 /** ROUTES PLANNER */
-router.get("/user/:id") // récupère tous les tableaux d'un user
-router.get ("/:id") //récupère un tableau via son id avec ses données liées 
-router.put("/:id") // modifie un tableau
-router.post("/:id") // ajoute un tableau
-router.delete("/:id") // supprime un tableau
+router.get("/user/:id", plannerController) // récupère tous les tableaux d'un user
+router.get ("/:id", plannerController) //récupère un tableau via son id avec ses données liées 
+router.put("/:id", plannerController) // modifie un tableau
+router.post("/:id", plannerController) // ajoute un tableau
+router.delete("/:id", plannerController) // supprime un tableau
 
 /** ROUTES TASK */
-router.get("/planner/:id/user/:id") // récupère les tâches d'un user dans un tableau
-router.post("/planner/post") // ajoute un tâche dans un tableau
-router.put("/:id/planner/:id") // modifie une tâche dans un tableau
-router.delete("/:id/planner/:id") // supprime une tâche dans un tableau
-
+router.get("/planner/:id/user/:id", taskController) // récupère les tâches d'un user dans un tableau
+router.post("/planner/post", taskController) // ajoute un tâche dans un tableau
+router.put("/:id/planner/:id", taskController) // modifie une tâche dans un tableau
+router.delete("/:id/planner/:id", taskController) // supprime une tâche dans un tableau
 
 /** ROUTES CATEGORIES */
-router.get("/") // récupère toutes les catégories
-router.get("/:id/planner/:id/task/") //récupère les tâches d’une catégorie dans un planner
-router.post("/") //créer une catégorie
-router.delete("/:id") //supprimer une catégorie
-router.put("/:id") //modifier une catégorie
+router.get("/", categoryController) // récupère toutes les catégories
+router.get("/:id/planner/:id/task/", categoryController) //récupère les tâches d’une catégorie dans un planner
+router.post("/", categoryController) //créer une catégorie
+router.delete("/:id", categoryController) //supprimer une catégorie
+router.put("/:id", categoryController) //modifier une catégorie
 
 
 module.exports = router;
