da2fca13388073c31f2eacf454dd5de3996a8aff
BarbaraOuisse <barbaraouisse@hotmail.com>
2023-02-23 14:11:59 +0100

    format date faker

--- data/seeding.js
+++ data/seeding.js
@@ -10,12 +10,13 @@ const NB_USER = 30;
 function generateUser(nbUsers) {
     const users = [];
     for (let iUser= 0; iUser < nbUsers; iUser +=1){
-        const birthdateUser = faker.date.birthdate()
+        const birthdateUser = faker.date.birthdate();
         const user = {
             firstname: faker.name.firstName(),
             lastname: faker.name.lastName(), 
             email:faker.internet.email(),
-            birthdate:birthdateUser.toLocaleDateString('fr-FR', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit'}),
+            birthdate: faker.date.birthdate(),
+            //birthdateUser.toLocaleDateString('fr-FR', { year: 'numeric',month: '2-digit',day: '2-digit',  , hour: '2-digit', minute: '2-digit', second: '2-digit'}),
             avatar:faker.image.cats(),
             password:faker.internet.password(),
             color:faker.color.rgb(),
