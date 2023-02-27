c3e8f169c72e00a76ef3eab0140978e44e8bebe3
Fetra RABEHASY <94452120+Supafei@users.noreply.github.com>
2023-02-27 14:57:57 +0100

    methode GET, delete et GET user planner

--- app/controller/plannerController.js
+++ app/controller/plannerController.js
@@ -1,25 +1,42 @@
 const dataMapper = require('../datamapper');
 
 const plannerController = {
-// récupère tous les tableaux d'un user
-    async getUserPlanners () {
+    // récupère tous les tableaux d'un user
+    async getUserPlanners(_, response) {
+
+        let getAllPlanners = await dataMapper.getAll("planner");
+        console.log(getAllPlanners);
+
+        return response.json(getAllPlanners);
+    },
+    //récupère un tableau via son id avec ses données liées 
+    async getPlanner(request, response) {
+
+        let plannerId = request.params.id;
+        let getPlanner = await dataMapper.getOneById("planner", plannerId);
+        return response.json(getPlanner);
 
     },
-//récupère un tableau via son id avec ses données liées 
-    async getPlanner () {
+    // modifie un tableau
+    async updatePlanner(request, response) {
 
     },
-// modifie un tableau
-    async updatePlanner () {
+    // ajoute un tableau
+    async createPlanner() {
 
     },
-// ajoute un tableau
-    async createPlanner () {
+    // supprime un tableau
+    async deletePlanner() {
+
+        let plannerId = request.params.id;
+        let deletePlanner = await dataMapper.deleteOne("planner", plannerId);
+
+        console.log(`nombre de ligne supprimée: ${deletePlanner.rowCount}`);
+        return response.json(deletePlanner);
 
     },
-// supprime un tableau
-    async deletePlanner () {
-        
+    async getCategoryTasks() {
+
     }
 
 }
--- app/middleware/middlewareLogin.js
+++ app/middleware/middlewareLogin.js
@@ -8,21 +8,21 @@ const middlewareLogin = {
 
     next();
   },
-  authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
+  // authenticateToken(req, res, next) {
+  //   const authHeader = req.headers['authorization']
+  //   const token = authHeader && authHeader.split(' ')[1]
 
-    if (token == null) return res.sendStatus(401)
+  //   if (token == null) return res.sendStatus(401)
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-      if (err) {
-        return res.sendStatus(401)
-      }
-      req.user = user;
-      next();
-    });
+  //   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+  //     if (err) {
+  //       return res.sendStatus(401)
+  //     }
+  //     req.user = user;
+  //     next();
+  //   });
 
-  }
+  // }
 }
 
 module.exports = middlewareLogin;
