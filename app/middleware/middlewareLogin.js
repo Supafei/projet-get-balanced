d8b602727e0dada742af5364d5a38311b67accd5
Fetra RABEHASY <94452120+Supafei@users.noreply.github.com>
2023-03-02 12:13:39 +0100

    modif checkTOken2

--- app/middleware/middlewareLogin.js
+++ app/middleware/middlewareLogin.js
@@ -22,7 +22,7 @@ const middlewareLogin = {
 
   checkToken(request, response, next) {
     try {
-      console.log(request.headers.authorization.split(" ")[1]);
+      console.log('envoyé par le front: ', request.headers.authorization.split(" ")[1]);
 
       // on vérifie qu'il y a le token dans le headers authorization
       const token = request.headers.authorization.split(" ")[1];
