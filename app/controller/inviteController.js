cc06b4a8ff371ec9b5efca875ddafb176476dce6
BarbaraOuisse <barbaraouisse@hotmail.com>
2023-03-10 14:07:28 +0100

    test email

--- app/controller/inviteController.js
+++ app/controller/inviteController.js
@@ -18,21 +18,28 @@ const inviteController = {
         console.log(request.body);
 
         let emailToInvite = request.body.email;
-        console.log("email à inviter",emailToInvite);
+        console.log("email à inviter", emailToInvite);
         let userHostId = request.params.userId;
         console.log("user Id de l'inviteur", userHostId);
         let plannerId = request.params.plannerId;
 
+        // Je récupère les infos de l'utilisateur qui envoie l'invitation
         let getUserData = await dataMapper.getOneById("\"user\"", "id", userHostId);
 
-        let findGuest = await dataMapper.getByCondition("\"user\"", "email", emailToInvite);
+        // je vérifie si la personne invitée a un compte
+        let findGuest = await dataMapper.getOneByCondition("\"user\"", "email", emailToInvite);
 
+
+        // je récupère les infos du planner
         let getPlanner = await dataMapper.getOneById("planner", "id", plannerId);
+
 
         let emailContent;
 
+        console.log(findGuest);
         // si l'utilisateur existe en bdd
         if (findGuest) {
+
 
             //j'ajoute l'authorisation dans la table user_has_planner
             let giveAuthorization = await dataMapper.insertOne({
@@ -44,7 +51,7 @@ const inviteController = {
 
 
             // si l'utilisateur n'existe pas
-        } else {
+        } else if (!findGuest){
             // j'enregistre l'authorisation dans la table invite (pour plus tard quand l'utilisateur sera inscrit)
             let giveAuthorization = await dataMapper.insertOne({
                 user_email: emailToInvite,
